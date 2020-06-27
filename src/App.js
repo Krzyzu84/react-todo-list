dfd2e68d345ff914f8a7a2c4b5a5238b368c46e5
unknown <krzysztofptak97@gmail.com>
2020-06-27 15:05:39 +0200

    App Component Structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
-import React from "react";
-import uuid from "uuid";
+import React, { Component } from "react";
+import { v4 as uuid } from "uuid";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
-//showing vs-code
+export default class App extends Component {
+  state = {
+    items: [
+      {
+        id: 1,
+        title: "wake up",
+      },
+      {
+        id: 2,
+        title: "make breakfast",
+      },
+      {
+        id: 3,
+        title: "sleep",
+      },
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false,
+  };
 
-function App() {
-  return (
-    <React.Fragment>
-      <div className="container">
-        <div className="row">
-          <TodoInput />
-          <TodoList />
+  handleChange = (e) => {
+    console.log("handle change");
+  };
+  handleSubmit = (e) => {
+    console.log("handle Submit");
+  };
+
+  clearList = () => {
+    console.log("clear list");
+  };
+  handleDelete = (id) => {
+    console.log("handleDelete");
+  };
+  handleEdit = (id) => {
+    console.log("handleEdit");
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="row">
+            <div class="col-10 mx-auto col-md-8 mt-5">
+              <h3 className="text-capitalize text-center"></h3>
+              <TodoInput
+                item={this.state.item}
+                handleChange={this}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              />
+              <TodoList
+                items={this.state.items}
+                clearList={this.clearList}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
-
-export default App;
