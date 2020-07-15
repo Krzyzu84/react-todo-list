b684c926ff1ad7be327de9792b74fb92e735b1df
unknown <krzysztofptak97@gmail.com>
2020-07-15 15:51:41 +0200

    fixed uuid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from "react";
-import { v4 as uuid } from "uuid";
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-
-export default class App {
+class App extends Component {
   state = {
     items: [],
-    id: uuid(),
+    id: uuidv4(),
     item: "",
     editItem: false,
   };
-
   handleChange = (e) => {
     this.setState({
       item: e.target.value,
@@ -22,20 +21,15 @@ export default class App {
       id: this.state.id,
       title: this.state.item,
     };
-
     const updatedItems = [...this.state.items, newItem];
 
-    this.setState(
-      {
-        items: updatedItems,
-        item: "",
-        id: uuid(),
-        editItem: false,
-      },
-      () => console.log(this.state)
-    );
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuidv4(),
+      editItem: false,
+    });
   };
-
   clearList = () => {
     this.setState({
       items: [],
@@ -57,13 +51,12 @@ export default class App {
       editItem: true,
     });
   };
-
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-5">
-            <h3 className="text-capitalize text-center"></h3>
+            <h3 className="text-capitalize text-center">todo input</h3>
             <TodoInput
               item={this.state.item}
               handleChange={this.handleChange}
@@ -82,3 +75,5 @@ export default class App {
     );
   }
 }
+
+export default App;
