f118f7251953e59face8bf08042d16e207baa8f0
unknown <krzysztofptak97@gmail.com>
2020-07-15 15:38:24 +0200

    fix bug deply netfily

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import { v4 as uuid } from "uuid";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
-export default class App extends Component {
+export default class App {
   state = {
     items: [],
     id: uuid(),
@@ -60,27 +60,25 @@ export default class App extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <div className="container">
-          <div className="row">
-            <div className="col-10 mx-auto col-md-8 mt-5">
-              <h3 className="text-capitalize text-center"></h3>
-              <TodoInput
-                item={this.state.item}
-                handleChange={this.handleChange}
-                handleSubmit={this.handleSubmit}
-                editItem={this.state.editItem}
-              />
-              <TodoList
-                items={this.state.items}
-                clearList={this.clearList}
-                handleDelete={this.handleDelete}
-                handleEdit={this.handleEdit}
-              />
-            </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center"></h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
           </div>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
